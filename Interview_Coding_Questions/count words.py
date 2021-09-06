3a9bf4bc2e2b9e10300958790ef007186a4c2780
Reetam Dutta <reetamdutta1@gmail.com>
2021-09-06 21:28:36 +0530

    Add files via upload

---
+++ Interview_Coding_Questions/count words.py
@@ -0,0 +1,16 @@
+def wordCount(str1):
+    p = str1.split(" ")
+    r = len(p)
+    return r
+
+# { Driver Code Starts
+if __name__ == '__main__':
+        str1 = input()
+        print(wordCount(str1))
+# } Driver Code Ends
+
+
+
+
+
+

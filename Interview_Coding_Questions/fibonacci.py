64e5d4ff331b70af1630dc9001deecc6b02256ae
Reetam Dutta <reetamdutta1@gmail.com>
2021-09-11 11:34:18 +0530

    Add files via upload

--- Interview_Coding_Questions/fibonacci.py
+++ Interview_Coding_Questions/fibonacci.py
@@ -1,15 +1,20 @@
 def fibo(n):
-    if n<0:
-        return -1
-    
-    elif n ==0:
+    if n < 0:
+        return("INVALID INPUT")
+    elif n == 0:
         return 0
-        
-    elif n ==1 or n==2:
+    elif n ==1 or n ==2:
         return 1
+    else:
+        return fibo(n-1) + fibo(n-2)
         
-    while(n>2):
-        return (fibo(n-1)+fibo(n-2))
-        
+if __name__ =='__main__':
+    output = []
+    n = int(input())
+    for n in range(0,n+1):
+        output.append(fibo(n))
+    print(output)
 
-print(fibo(9))
+
+
+

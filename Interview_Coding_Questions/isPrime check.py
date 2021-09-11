bf6a5fdbbe8ed4151d018d020ec4db35526a2adb
Reetam Dutta <reetamdutta1@gmail.com>
2021-09-11 11:33:18 +0530

    Add files via upload

---
+++ Interview_Coding_Questions/isPrime check.py
@@ -0,0 +1,14 @@
+# Taking the input from User 
+number = int(input("Enter The Number: "))
+
+if number > 1:
+    for i in range(2,int(number/2)+1):
+        if (number % i == 0):
+            print(number, "is not a Prime Number")
+            break
+    else:
+        print(number,"is a Prime number")
+# If the number is less than 1 it can't be Prime    
+else:
+    print(number,"is not a Prime number")
+

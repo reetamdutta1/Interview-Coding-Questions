b4a8f46947a00d7eb0c70e73445c8539ca7f61dd
Reetam Dutta <reetamdutta1@gmail.com>
2021-09-12 19:00:57 +0530

    Add files via upload

--- Interview_Coding_Questions/armstrong number.py
+++ Interview_Coding_Questions/armstrong number.py
@@ -1,24 +1,21 @@
-#User function Template for python3
-class Solution:
-    def armstrongNumber (ob, n):
-        # code here 
-        a = int(n%10)
-        b = int((n/10)%10)
-        c = int(n/100)
-        
-        if((a*a*a + b*b*b + c*c*c) == n):
-            return "Yes"
-        else:
-            return "No"
+def armstrongNumber (n):
+    # code here 
+    a = int(n%10) #unit's place digit
+    #print(a)
+    b = int((n/10)%10) #ten's place digit
+    #print(b)
+    c = int(n/100) #100's place digit
+    #print(c)
+    
+    if((a*a*a + b*b*b + c*c*c) == n):
+        return "YES an Armstrong Number"
+    else:
+        return "NOT an Armstrong Number"
 
-#{ 
+# { 
 #  Driver Code Starts
-#Initial Template for Python 3
 if __name__ == '__main__': 
-    t = int (input ())
-    for _ in range (t):
-        n = input()
-        n=int(n)
-        ob = Solution()
-        print(ob.armstrongNumber(n))
+        n = int(input())
+        print(armstrongNumber(n))
 # } Driver Code Ends
+

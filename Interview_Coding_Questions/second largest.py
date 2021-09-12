d2a871685018db4a7afe25e740f87adc67d1912c
Reetam Dutta <reetamdutta1@gmail.com>
2021-09-12 19:00:42 +0530

    Add files via upload

--- Interview_Coding_Questions/second largest.py
+++ Interview_Coding_Questions/second largest.py
@@ -6,15 +6,15 @@ def find2ndLargest(n):
         ele = int(input())
         user.append(ele)
 
-    new_list = set(user)
+    new_list = set(user) #to get distinct values
+    print(new_list)
     new_list.remove(max(new_list))
-    return('2nd Largest number is: {}'.
-                    format(max(new_list)) )
+    return('2nd Largest number is: {}'.format(max(new_list)) )
 
 # { Driver Code Starts
+#Initial Template for Python 3
+
 if __name__ == '__main__':
         n = int(input())
         print(find2ndLargest(n))
 # } Driver Code Ends
-
-

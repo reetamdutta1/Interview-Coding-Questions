e0d84357b3428aff2f76df1b62113493f7213371
Reetam Dutta <reetamdutta1@gmail.com>
2021-09-14 00:23:22 +0530

    Add files via upload

--- Interview_Coding_Questions/replace character.py
+++ Interview_Coding_Questions/replace character.py
@@ -3,11 +3,13 @@ def replaceChr(str1,a,b):
     return str2
 
 # { Driver Code Starts
+
 if __name__ == '__main__':
         str1 = input("Enter your string: ")
         a = input("Input character to replace: ")
         b = input("Input New character: ")
         print("New string: ",replaceChr(str1,a,b))
+
 # } Driver Code Ends
 
 
